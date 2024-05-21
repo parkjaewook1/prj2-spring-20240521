c624aace64235fa20a36ac9c512fd3d9cceb78a6
jaewook <wodnr5050@gmail.com>
2024-05-21 12:11:21 +0900

    BoardService, BoardMapper 작성 board 테이블 생성 쿼리

--- src/main/java/com/prj2spring20240521/controller/board/BoardController.java
+++ src/main/java/com/prj2spring20240521/controller/board/BoardController.java
@@ -1,6 +1,7 @@
 package com.prj2spring20240521.controller.board;
 
 import com.prj2spring20240521.domain.board.Board;
+import com.prj2spring20240521.service.board.BoardService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/board")
 public class BoardController {
+
+    private final BoardService service;
+
     @PostMapping("add")
     public void add(@RequestBody Board board) {
-        System.out.println("board = " + board);
+        service.add(board);
     }
 }
