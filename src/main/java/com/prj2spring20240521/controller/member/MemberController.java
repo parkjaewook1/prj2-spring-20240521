9be583f9e34eb942f2247ab5cd5aeeeb3f72e5cb
jaewook <wodnr5050@gmail.com>
2024-05-22 11:10:34 +0900

    별명 사용 가능한 지 확인 404 -> 없으니까 사용 가능 200 -> 있으니까 사용 불가능

--- src/main/java/com/prj2spring20240521/controller/member/MemberController.java
+++ src/main/java/com/prj2spring20240521/controller/member/MemberController.java
@@ -29,8 +29,14 @@ public class MemberController {
     }
 
     @GetMapping(value = "check", params = "nickName")
-    public void checkNickName(@RequestParam("nickName") String nickName) {
-        System.out.println("nickName = " + nickName);
+    public ResponseEntity checkNickName(@RequestParam("nickName") String nickName) {
+        Member member = service.getByNickName(nickName);
+
+        if (member == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(nickName);
     }
+
 
 }
