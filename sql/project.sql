4178b2b97d3e8dd85ccb0be9c014a331e0b8a48d
jaewook <wodnr5050@gmail.com>
2024-05-30 10:21:33 +0900

    게시물 목록에 좋아요 갯수(조인

--- sql/project.sql
+++ sql/project.sql
@@ -3,7 +3,7 @@ USE prj2;
 # 게시물 테이블 생성
 CREATE TABLE board
 (
-    id       INT PRIMARY KEY KEY AUTO_INCREMENT,
+    id       INT PRIMARY KEY AUTO_INCREMENT,
     title    VARCHAR(100)  NOT NULL,
     content  VARCHAR(1000) NOT NULL,
     writer   VARCHAR(100)  NOT NULL,
@@ -15,22 +15,24 @@ FROM board
 ORDER BY id DESC;
 
 
+# member table 만들기
 CREATE TABLE member
 (
     id        INT PRIMARY KEY AUTO_INCREMENT,
     email     VARCHAR(100) NOT NULL UNIQUE,
     password  VARCHAR(100) NOT NULL,
     nick_name VARCHAR(100) NOT NULL UNIQUE,
+
     inserted  DATETIME     NOT NULL DEFAULT NOW()
 );
 
 SELECT *
-FROM member
-ORDER BY id ASC;
+FROM member;
 
 # board 테이블 수정
 # writer column 지우기
 # member_id column reference member(id)
+
 ALTER TABLE board
     DROP COLUMN writer;
 DESC board;
@@ -43,16 +45,23 @@ WHERE id > 0;
 
 ALTER TABLE board
     MODIFY COLUMN member_id INT NOT NULL;
-
 DESC board;
-
 SELECT *
 FROM board
 ORDER BY id DESC;
 
 SELECT *
-FROM member;
+FROM member
+WHERE email = 'qq@qq';
 
+DELETE
+FROM board
+WHERE member_id = 9;
+DELETE
+FROM member
+WHERE email = 'qq@qq';
+
+# 권한 테이블
 CREATE TABLE authority
 (
     member_id INT         NOT NULL REFERENCES member (id),
@@ -61,29 +70,37 @@ CREATE TABLE authority
 );
 
 INSERT INTO authority (member_id, name)
-VALUES (11, 'admin');
+VALUES (19, 'admin');
+
 
 INSERT INTO board
     (title, content, member_id)
 SELECT title, content, member_id
 FROM board;
 
+USE prj2;
+
+# 게시물 여러개 입력
+INSERT INTO board
+    (title, content, member_id)
+SELECT title, content, member_id
+FROM board;
+SELECT COUNT(*)
+FROM board;
 SELECT *
 FROM member;
-
 UPDATE member
 SET nick_name = 'abcd'
-WHERE id = 11;
-
+WHERE id = 18;
 UPDATE member
 SET nick_name = 'efgh'
-WHERE id = 12;
+WHERE id = 19;
 
 UPDATE board
-SET member_id = 11
+SET member_id = 18
 WHERE id % 2 = 0;
 UPDATE board
-SET member_id = 12
+SET member_id = 19
 WHERE id % 2 = 1;
 
 UPDATE board
@@ -110,9 +127,10 @@ CREATE TABLE board_file
 );
 
 SELECT *
-FROM board_file;
+FROM board_file
+WHERE board_id = 878;
 
-
+# board_like 만들기
 CREATE TABLE board_like
 (
     board_id  INT NOT NULL REFERENCES board (id),
@@ -121,4 +139,17 @@ CREATE TABLE board_like
 );
 
 SELECT *
-FROM board_like;
+FROM member;
+
+SELECT b.id, COUNT(DISTINCT f.name), COUNT(DISTINCT l.member_id)
+FROM board b
+         JOIN member m ON b.member_id = m.id
+         LEFT JOIN board_file f ON b.id = f.board_id
+         LEFT JOIN board_like l ON b.id = l.board_id
+WHERE b.id = 5;
+
+
+
+
+
+
